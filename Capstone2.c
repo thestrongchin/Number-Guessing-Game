eb7a8a75f03925202ec6ab89fb56ec82362f1e5a
River Chen <riverchen0510@gmail.com>
2025-07-21 19:33:12 +0800

    updated

--- Capstone2.c
+++ Capstone2.c
@@ -5,39 +5,35 @@
 #define DEBUG
 #define MAX_ATTEMPTS = 10;
 
-int num = 0;
+int generateNum = 0; // Global Variable
+int guesses = 5
 
 int main(){
-    int generateNum;
-    int guesses;
-
     srand(time(0));
     generateNum = rand() % 100 + 1;
-}
 
-int get_user_guess(int guess){
-    while(1){
-    int attempts == MAX_ATTEMPTS;
+       while(1){
+    int attempts = MAX_ATTEMPTS;
 
-    if(guess >= 1){
+    if(guesses >= generateNum){
 
-        if(guess < num){
+        if(guesses < generateNum){
             printf("Too low!");
-            printf("Guesses remaining: %d \n", MAX_ATTEMPTS);
+            printf("Guesses remaining: %d \n", attempts);
+            guesses--;
         }
     }
 
-        if(guess > num){
+        if(guesses > generateNum){
             printf("Too high!");
             printf("Guesses remaining: %d \n", MAX_ATTEMPTS);
         }
 
         if 
 
-        if(guess <= 0){
+        if(guesses <= 0 ){
             printf("invalid guess, please try again.");
             printf("Guesses remaining: %d \n", MAX_ATTEMPTS);
         }
-
     }
 }
