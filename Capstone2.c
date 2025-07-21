fc0b8a1913771c14b262985fe2b1b3873a6288e6
River Chen <riverchen0510@gmail.com>
2025-07-21 19:42:22 +0800

    fixed more

--- Capstone2.c
+++ Capstone2.c
@@ -3,16 +3,21 @@
 #include <time.h>
 
 #define DEBUG
-#define MAX_ATTEMPTS = 10;
+#define MAX_ATTEMPTS 10
 
 int generateNum = 0; // Global Variable
-int guesses = 5
+int guess;
+int attempts;
 
 int main(){
     srand(time(0));
     generateNum = rand() % 100 + 1;
 
-       while(1){
+    printf("===========================================================");
+    printf("            Welcome to the number guessing game!             ");
+    printf("===========================================================")
+
+    while(0){
     int attempts = MAX_ATTEMPTS;
 
     if(guesses >= generateNum){
