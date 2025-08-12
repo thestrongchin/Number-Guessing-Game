44a607be1e19a35032d1081affc03a087f1ec5f9
River Chen <riverchen0510@gmail.com>
2025-08-12 12:40:18 +0800

    updates

--- main.c
+++ main.c
@@ -11,81 +11,70 @@ int attempts;
 int TotalAttempts = 0;
 
 int main(){
-
-    start:
     
     char selection;
 
-    srand(time(0)); // This creates a seed for the number generator
-    num = rand() % 100 + 1; // This generates a random number between 1 and 100 and assigns it to variable "num"
+    while(1){
 
-    printf("=============================================== \n");
-    printf("      Welcome to the number guessing game! \n");
-    printf("===============================================\n");
+        srand(time(0)); // This creates a seed for the number generator
+        num = rand() % 100 + 1; // This generates a random number between 1 and 100 and assigns it to variable "num"
 
-    int attempts = MAX_ATTEMPTS;
+        printf("=============================================== \n");
+        printf("      Welcome to the number guessing game! \n");
+        printf("===============================================\n");
 
-    for(attempts = 10; attempts > 0; attempts--){ // Assigns 10 value to attempts, attempts must be over 0, and attempts's value will decrease by 1 each time. Keeps looping until condition is not met.
+        int attempts = MAX_ATTEMPTS;
 
-    printf("Please guess a number: ");
-    scanf("%d", &guess);
 
-        if(guess >= 1 && guess < num){
-            printf("Guess too low! \n");
-            printf("You have %d attempts left: \n \n", attempts);
+        for(attempts = 10; attempts > 0; attempts--){ // Assigns 10 value to attempts, attempts must be over 0, and attempts's value will decrease by 1 each time. Keeps looping until condition is not met.
 
-            TotalAttempts++; // Adds 1 to 'TotalAttempts' each time user guesses
+            printf("Please guess a number: ");
+            scanf("%d", &guess);
+
+                if(guess >= 1 && guess < num){
+                    printf("Guess too low! \n");
+                    printf("You have %d attempts left: \n \n", attempts);
+
+                    TotalAttempts++; // Adds 1 to 'TotalAttempts' each time user guesses
+                }
+
+                if(guess >= 1 && guess > num){
+                    printf("Guess too high! \n");
+                    printf("You have %d attempts left: \n \n", attempts);
+
+                    TotalAttempts++;
+                } 
+
+                
+                if(guess <= 0 || guess > 100){
+
+                    printf("Invalid input, please try again \n"); 
+
+                }
         }
 
-        if(guess >= 1 && guess > num){
-            printf("Guess too high! \n");
-            printf("You have %d attempts left: \n \n", attempts);
+            if(guess == num){
 
-            TotalAttempts++;
-        } 
+                printf("Congratulations! you have guessed the correct number! \n");
+                printf("You have guessed the number in %d attempts \n \n", TotalAttempts);
+            }
 
+            if(attempts == 0){
+                printf("You have run out of attempts! \n");
+                printf("The correct number was: %d \n \n", num);
+            }
 
+            if(selection == 'Y' || selection == 'y'){
+            }
 
-        
-        if(guess <= 0 || guess > 100){
+            if(selection == 'N' || selection == 'n'){
+                printf("Thanks for playing the Number Guessing Game! Come again soon! \n");
+                return 0;
 
-            printf("Invalid input, please try again \n"); 
-            attempts++; // Adds 1 to attempts becuase user put in an invalid input
-
-        }
-    }
-
-    while(0){
-
-        if(guess == num){
-
-            printf("Congratulations! you have guessed the correct number! \n");
-            printf("You have guessed the number in %d attempts \n \n", TotalAttempts);
-        }
-
-        if(attempts == 0){
-            printf("You have run out of attempts! \n");
-            printf("The correct number was: %d \n \n", num);
-        }
-
-        printf("Would you like to play again? \n");
-        printf("Yes = 'Y' or 'y' \n");
-        printf("No = 'N' or 'n' \n");
-        printf("Please make your selection: ");
-
-        scanf(" %c", &selection);
-
-        if(selection == 'Y' || selection == 'y'){
-            goto start;
-        }
-
-        if(selection == 'N' || selection == 'n'){
-            printf("Thanks for playing the Number Guessing Game! Come again soon! \n");
-            return 0;
-
-        } else {
-            printf("invalid input, game terminating... \n");
-            return 0;
-        }
+            } else {
+                printf("invalid input, game terminating... \n");
+                return 0;
+            }
+    
     }
 }
